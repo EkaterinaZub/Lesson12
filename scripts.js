4047c98243c20eed6fa941e51c517ea113069e2f
EkaterinaZub <katenka.zu@gmail.com>
2023-02-26 18:18:45 +0300

    add: task added number 9,10

--- scripts.js
+++ scripts.js
@@ -67,4 +67,70 @@ console.log(a1, typeof a18);
 
   const VCilindra = pi*r**2*h;
 
-console.log(VCilindra.toFixed(2));
+console.log(VCilindra.toFixed(2)+ " м");
+
+//  Задача №3
+
+let width = 10;
+    height = 23;
+
+const SPryam =  width * height;
+console.log(SPryam + " см")
+
+// Задача №5
+// Найдите площадь круга (SKruga) с радиусом 5см (r).
+
+let radius = 5;
+ const SKruga = pi*radius**2;
+
+ console.log(SKruga + " см");
+
+// Задача №6
+//  Найдите площадь трапеции (STrap) с основаниями 5см (c) и 7см (b), и высотой 10см
+// (h).
+
+let c = 5;
+    b = 7;
+    h = 10;
+
+const STrap = c + d / 2* h;
+console.log(STrap + " см")  
+
+// Задача №7
+// Даны: размер ипотечного кредита (S - 2 млн. руб), процентная годовая ставка (p - 10%),
+// кол-во лет (years - 5). Найти переплату по кредиту, значение переплаты должно
+// содержаться в переменной Pereplata.
+
+let S = 2000000;
+    p = 0.1;
+    years = 5;
+
+    NoInterest = S/years;
+    Year1Interest =  S*p;
+    Year2Interest = (S-NoInterest)*p;
+    Year3Interest = (S-2*NoInterest)*p;
+    Year4Interest = (S-3*NoInterest)*p;
+    Year5Interest = (S-4*NoInterest)*p;
+const Pereplata = Year1Interest + Year2Interest + Year3Interest + Year4Interest + Year5Interest;
+
+
+console.log(Pereplata)
+
+// Задача №8
+// Решите уравнения (найдите неизвестный x), где a = 8, b = 3:
+// a+2(x-b)=16;
+// b(x+15)=a+6x;
+// x+2x+ax+bx=23780.
+
+let a = 8;
+    b = 3;
+
+    const example1 = (16+2*b-a)/2;
+    const example2 = (15*b-a)/(6-b);
+    const example3 = 23780/(1+2+a+b);
+    
+    console.log(example1);
+    console.log(example2.toFixed(3));
+    console.log(example3.toFixed(2));
+
+
